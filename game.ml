c5b70b5bdf2f9572efdc837ab664660da32f6421
Jackson Kopitz <jsk363@cornell.edu>
2020-04-19 19:08:01 -0700

    worked on json parser

--- game.ml
+++ game.ml
@@ -1,1 +1,22 @@
 open Yojson.Basic.Util
+
+type coordinate = (int * int)
+
+type orientation = {
+  orient_name : string;
+  coordinates : coordinate list
+}
+
+type shape = {
+  shape_name : string;
+  color : int;
+  orientations : orientation list;
+}
+
+type t = {
+  shapes : shape list
+}
+
+let orientation_of_json j = {
+  orient_name = j |> member "";
+}
