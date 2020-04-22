91247610ff6ae3d2b8718da201daa865c9eadc23
Jackson Kopitz <jsk363@cornell.edu>
2020-04-22 13:33:17 -0700

    state mli

---
+++ state.mli
@@ -0,0 +1,21 @@
+open Graphics
+open Board
+open Game
+
+type t 
+
+val init_state : Game.t -> t
+
+val blockref_x
+
+val blockref_y 
+
+val add_blockref 
+
+val won : st -> bool
+
+val render_moving 
+
+val update : t-> 
+
+val rotate 
